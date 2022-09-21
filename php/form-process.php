bbacb5e222a641ddaeeaab032e69f8d212fcb76b
arktony <arktumwesigye@gmail.com>
2022-09-21 22:51:25 +0300

    first commit

---
+++ php/form-process.php
@@ -0,0 +1,88 @@
+<?php
+
+$errorMSG = "";
+
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
+
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG .= "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+// PHONE
+if (empty($_POST["phone"])) {
+    $errorMSG .= "phone is required ";
+} else {
+    $phone = $_POST["phone"];
+}
+
+//COMPANY
+if (empty($_POST["phone"])) {
+   
+} else {
+    $company = $_POST["company"];
+}
+
+
+// MSG SUBJECT
+if (empty($_POST["subject"])) {
+    $errorMSG .= "Subject is required ";
+} else {
+    $subject = $_POST["subject"];
+}
+
+
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "Message is required ";
+} else {
+    $message = $_POST["message"];
+}
+
+
+$EmailTo = "jovanibary@gmail.com";
+$Subject = "Enquiry From GYCU Website";
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Phone: ";
+$Body .= $phone;
+$Body .= "\n";
+$Body .= "Company: ";
+$Body .= $company;
+$Body .= "\n";
+$Body .= "Subject: ";
+$Body .= $subject;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+
+?>
